072fc985d02b4a016167e9ceb66a4c0b1b3d56d8
Andres <andresr774@gmail.com>
2021-12-24 14:11:27 -0500

    Initial Commit

---
+++ AdvancedLearning/AdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedLearningApp.swift
+//  AdvancedLearning
+//
+//  Created by Andres camilo Raigoza misas on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
